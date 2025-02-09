1d14794219dedbd78e572a9e8823e7f57eb99836
Gustav Lundgren <gustalun@kth.se>
2025-02-09 12:29:06 +0100

    login persistance

--- frontend/src/components/firebaseAuth.js
+++ frontend/src/components/firebaseAuth.js
@@ -1,5 +1,6 @@
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { reactive } from "vue";
+import router from "../router/index"
 
 // Create a reactive object to track the current user
 export const authState = reactive({
@@ -20,6 +21,11 @@ onAuthStateChanged(auth, (user) => {
     if (user) {
         console.log("User: " + user.uid);
         console.log(authState.initialized);
+    }
+
+
+    if (router.currentRoute.value.path == "/login") {
+        router.push("/leaderboard")
     }
 });
 
